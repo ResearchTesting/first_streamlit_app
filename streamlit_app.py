dcf8541dae4e12b574b573d06ebe5e4ec075262b
ResearchTesting <151973493+ResearchTesting@users.noreply.github.com>
2023-12-01 19:42:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.dataframe(fruits_to_show)
 
 # New Section to Display Fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','apple'(my_fruit_list.index)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','apple')
 streamlit.write('The user entered ', fruit_choice)
 
 import requests
