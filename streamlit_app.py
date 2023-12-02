04ce41fbed9f4e0d1946318251b5829a025064a7
ResearchTesting <151973493+ResearchTesting@users.noreply.github.com>
2023-12-01 19:55:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
 #streamlit.dataframe(my_fruit_list)
@@ -25,7 +25,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
-streamlit.dataframe(fruits_to_show)
+#streamlit.dataframe(fruits_to_show)
 
 # New Section to Display Fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
