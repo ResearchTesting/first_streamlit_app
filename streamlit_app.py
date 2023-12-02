4b3be32159022700d0694f30219455fccb3f8377
ResearchTesting <151973493+ResearchTesting@users.noreply.github.com>
2023-12-01 20:17:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,19 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+# New Section for added text box test
+fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', fruit_choice)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+# take json and normalize it 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output it in the screen as a table
+streamlit.dataframe(fruityvice_normalized)
+
+
+
+
 
