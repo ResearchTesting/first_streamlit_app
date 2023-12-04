a3afe3d4d6b7172dafe34a1f39f1ef7a6976abc7
ResearchTesting <151973493+ResearchTesting@users.noreply.github.com>
2023-12-04 17:32:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,6 @@ try:
     streamlit.error('Please select fruit to get information.')
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
-    fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
     streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
