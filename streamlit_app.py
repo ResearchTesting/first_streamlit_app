0bfb45ef3550cec51baf44e8e398b06409c77964
ResearchTesting <151973493+ResearchTesting@users.noreply.github.com>
2023-12-04 09:46:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,23 +44,10 @@ except URLError as e:
   streamlit.error()
 
 
-
-#streamlit.write('The user entered ', fruit_choice)
-
-# take json and normalize it 
-# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it in the screen as a table
-# streamlit.dataframe(fruityvice_normalized)
-
-#dont run anything while we troubleshoot
-streamlit.stop()
-
-#import snowflake.connector
-
 streamlit.header("The fruit load list contains:")
 #snowflake-related functions
 def get_fruit_load_list():
-  with my_cur = my_cnx.cursor() as my_cur:
+  with my_cnx.cursor() as my_cur:
   my_cur.execute("select * from fruit_load_list")
   return: my_cur.fetchall()
 
